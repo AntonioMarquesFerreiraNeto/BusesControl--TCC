54d47c25266a06ca6811b27ad4e5fabf5dbbe580
Antonio Marques <antoniomarquesfneto@gmail.com>
2022-09-01 17:34:19 -0300

    contrato

--- CabareCrud/Controllers/ContratoController.cs
+++ CabareCrud/Controllers/ContratoController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BusesControl.Controllers {
     public class ContratoController : Controller {
         public IActionResult Index() {
-            ViewData["Title"] = "Contratos";
+            ViewData["Title"] = "Contratos ativos";
             return View();
+        }
+        public async Task<IActionResult> NovoContrato() {
+            ViewData["Title"] = "Novo contrato";
+            return View();
+        }
+
+        public async Task<IActionResult> Desabilitados() {
+            ViewData["Title"] = "Contratos inativos";
+            return View("Index");
         }
     }
 }
--- CabareCrud/Models/ValidacoesDados/ModelValidarDate/ValidarData.cs
+++ CabareCrud/Models/ValidacoesDados/ModelValidarDate/ValidarData.cs
@@ -12,13 +12,24 @@ namespace BusesControl.Models.ValidacoesCliente.ModelValidarDate {
         public bool ValidarDataNascimento(string data) {
             DateTime dataNascimento = DateTime.Parse(data);
             DateTime dataAtual = DateTime.Now;
-
             long dias = (int)dataAtual.Subtract(dataNascimento).TotalDays;
             long idade = dias / 365;
             if (idade < 18 || idade > 132) {
                 return false;
             }
-            return true;
+            else {
+                string year = dataNascimento.Year.ToString();
+                return ValidarAno(year);
+            }
+        }
+
+        public bool ValidarAno(string ano) {
+            if (ano.Length < 4 || ano.Length > 4) {
+                return false;
+            }
+            else {
+                return true;
+            }
         }
     }
 }
