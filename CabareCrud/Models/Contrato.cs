ba2431c6146e299b2be116c432d7eb9996600e76
Antonio Marques <antoniomarquesfneto@gmail.com>
2022-09-21 19:29:27 -0300

    refatoração

--- CabareCrud/Models/Contrato.cs
+++ CabareCrud/Models/Contrato.cs
@@ -1,16 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BusesControl.Models.ValidacoesDados.ModelValidarNum;
+using System.ComponentModel.DataAnnotations;
 
 namespace BusesControl.Models {
     public class Contrato {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório!")]
+        [ValidarCampo(ErrorMessage = "Campo obrigatório!")]
         public int IdMotorista { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório!")]
+        [ValidarCampo(ErrorMessage = "Campo obrigatório!")]
         public int IdCliente { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório!")]
+        [ValidarCampo(ErrorMessage = "Campo obrigatório!")]
         public int IdOnibus { get; set; }
     }
 }
--- CabareCrud/Models/ViewModels/ModelsContrato.cs
+++ CabareCrud/Models/ViewModels/ModelsContrato.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace BusesControl.Models.ViewModels {
     public class ModelsContrato {
-        public int Id { get; set; }
+
         public List<Onibus> OnibusList { get; set; }
         public List<Funcionario> MotoristaList { get; set; }
         public List<PessoaFisica> ClienteFisicoList { get; set; }
         public List<PessoaJuridica> ClienteJuridicoList { get; set; }
-
         public Contrato Contrato { get; set; }  
+
         //Construtor vazio para poder istânciar um objeto na controller. 
         public ModelsContrato() { }
     }
